4af4d3ffcb60a320fa25545293dc47a9d6d74df0
Peter mwangi <konectifytechnologies@gmail.com>
2026-02-24 16:59:37 +0300

    updated middleware

--- app/Http/Middleware/DebugRequest.php
+++ app/Http/Middleware/DebugRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,10 +17,33 @@ class DebugRequest
      */
     public function handle(Request $request, Closure $next): Response
     {
-        Log::info('Request reached Laravel', [
+         Log::info('=== SANCTUM DEBUG START ===', [
             'path' => $request->path(),
-            'cookies' => $request->cookies->all(),
-            'headers' => $request->headers->all(),
+
+            // Cookies
+            'xsrf_cookie' => $request->cookie('XSRF-TOKEN'),
+            'laravel_session_cookie' => $request->cookie('laravel_session'),
+
+            // Headers
+            'xsrf_header' => $request->header('X-XSRF-TOKEN'),
+            'origin' => $request->header('Origin'),
+            'referer' => $request->header('Referer'),
+
+            // Session
+            'session_id' => $request->session()->getId(),
+            'session_all' => $request->session()->all(),
+
+            // Auth
+            'auth_check' => Auth::check(),
+            'auth_user' => Auth::user(),
+
+            // HTTPS detection
+            'is_secure' => $request->isSecure(),
+            'scheme' => $request->getScheme(),
+            'full_url' => $request->fullUrl(),
+
+            // All headers
+            'all_headers' => $request->headers->all(),
         ]);
         return $next($request);
     }
